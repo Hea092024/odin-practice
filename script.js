8fff607563aa97cffa34cc37d66fa9583f50d8ff
Hector Acevedo <HEA092024@gmail.com>
2025-01-30 10:38:50 +0100

    .

--- script.js
+++ script.js
@@ -205,3 +205,8 @@ console.log(madeAString);
 // the join() function takes an array, joins
 // all the array items together into a single
 // string, and returns this new string
+
+const myNumber = Math.random();
+console.log(myNumber);
+// the random() function generates a random number between
+// 0 and up to but not including 1, and returns that number

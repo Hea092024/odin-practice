5b47bc2aa89132620b97dcf31decb0cd7c08207e
Hector Acevedo <HEA092024@gmail.com>
2025-01-30 10:35:38 +0100

    .

--- script.js
+++ script.js
@@ -191,3 +191,11 @@ module.exports = {
  }
 
  console.log(favoriteAnimal("cat"));
+
+ const myText = "I am a string";
+const newString = myText.replace("string", "sausage");
+console.log(newString);
+// the replace() string function takes a source string,
+// and a target string and replaces the source string,
+// with the target string, and returns the newly formed string
+
